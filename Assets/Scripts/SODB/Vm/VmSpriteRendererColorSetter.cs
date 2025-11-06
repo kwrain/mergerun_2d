a79b5af6d9dcedf2cb6b1cd2cb8702d434aa7c5f
kwrain <67406686+kwrain@users.noreply.github.com>
2025-11-06 22:53:28 +0900

    VmSpriteRendererColorSetter 추가

---
+++ Assets/Scripts/SODB/Vm/VmSpriteRendererColorSetter.cs
@@ -0,0 +1,38 @@
+using FAIRSTUDIOS.SODB.Property;
+using UnityEngine;
+using UnityEngine.UI;
+
+/**
+* VmGraphicColorSetter.cs
+* 작성자 : doseon@fairstudios.kr
+* 작성일 : 2022년 08월 18일 오전 9시 26분
+*/
+[AddComponentMenu("Vm/[Vm]SpriteRendererColorSetter")]
+[DisallowMultipleComponent]
+[SupportedProperty(typeof(PropertyColor), typeof(PropertyListColor), typeof(PropertyMapStringColor))]
+public class VmSpriteRendererColorSetter : VmBase<SpriteRenderer, VmSpriteRendererColorSetter.Param>
+{
+  public override void UpdateViewActivate()
+  {
+    view.color = GetValue(pInfos[0]);
+  }
+
+  public override void UpdateView(string context)
+  {
+    if(pInfos[0].Context != context) return;
+    UpdateViewActivate();
+  }
+
+  private Color GetValue(PropertyInfoBase pInfo) => pInfo.Property switch
+  {
+    PropertyColor p => p.NewValue,
+    PropertyListColor p => p[pInfo.Index],
+    PropertyMapStringColor p => p[pInfo.StringKey],
+    _ => Color.white,
+  };
+
+  public class Param : PropertyInfoParamBase
+  {
+    
+  }
+}
